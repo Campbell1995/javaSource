3978bb5c975b7370a0ce814680cf9482ea958a53
Campbell1995 <djp225400@163.com>
2020-09-06 17:29:26 +0800

    第一次提交

---
+++ src/com/campbell/test/HashMapTest.java
@@ -0,0 +1,14 @@
+package com.campbell.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Campbell
+ * @date 2020/7/23
+ */
+public class HashMapTest {
+
+    Map<String,Object> map = new HashMap<>();
+
+}
